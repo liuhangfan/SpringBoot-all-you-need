af29fcff4b461160ae16c10029862c9c9f45ba6d
Hangfan Liu <liuhangfan27@gmail.com>
2022-12-11 03:32:11 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringBoot-all-you-need
---
+++ src/test/java/com/liuhangfan/springbootallyouneed/SpringBootAllYouNeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuhangfan.springbootallyouneed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAllYouNeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
