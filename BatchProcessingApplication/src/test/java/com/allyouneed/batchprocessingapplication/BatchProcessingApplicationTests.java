c2e5d2e54be2abb14627c4669875c0b5851fedba
Hangfan Liu <liuhangfan27@gmail.com>
2022-12-29 23:43:30 -0800

    BatchProcessingApplication

---
+++ BatchProcessingApplication/src/test/java/com/allyouneed/batchprocessingapplication/BatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allyouneed.batchprocessingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
